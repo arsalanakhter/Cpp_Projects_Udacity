927f09c21c6d8871ce41aeea454fa630f145a9e5
Arsalan Akhter <aakhter@wpi.edu>
2020-04-06 07:47:38 -0400

    Added Projects Route Planning and System Monitor

---
+++ System_Monitor/include/system.h
@@ -0,0 +1,29 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <string>
+#include <vector>
+
+#include "process.h"
+#include "processor.h"
+#include "linux_parser.h"
+
+using namespace LinuxParser;
+class System {
+ public:
+  Processor& Cpu();                   // TODO: See src/system.cpp
+  std::vector<Process>& Processes();  // TODO: See src/system.cpp
+  float MemoryUtilization();          // TODO: See src/system.cpp
+  long UpTime();                      // TODO: See src/system.cpp
+  int TotalProcesses();               // TODO: See src/system.cpp
+  int RunningProcesses();             // TODO: See src/system.cpp
+  std::string Kernel() const;               // TODO: See src/system.cpp
+  std::string OperatingSystem() const;      // TODO: See src/system.cpp
+
+  // TODO: Define any necessary private members
+ private:
+  Processor cpu_ = {};
+  std::vector<Process> processes_ = {};
+};
+
+#endif
