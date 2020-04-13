636c1163986306d9237e3f06fa117d4bf9228579
Arsalan Akhter <aakhter@wpi.edu>
2020-04-13 11:09:47 -0400

    Task 1 done.

--- Memory_Management_Chatbot/src/chatgui.cpp
+++ Memory_Management_Chatbot/src/chatgui.cpp
@@ -118,7 +118,8 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    //_chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +136,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // Task 1. Comment following line because no longer needed
+    // delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
--- Memory_Management_Chatbot/src/chatgui.h
+++ Memory_Management_Chatbot/src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include<memory>
 
 class ChatLogic; // forward declaration
 
@@ -15,8 +16,8 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    // Task 1. Make _chatLogic unique to ChatBotPanelDialogue
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +28,8 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    // Task 1. Return raw pointer of the managed object
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
