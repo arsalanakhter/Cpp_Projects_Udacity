0400cee95fe7a6275077abe7a9ac8f149f92c712
Arsalan Akhter <aakhter@wpi.edu>
2020-04-22 11:58:55 -0400

    Task FP.1 done.

--- Concurrent_Traffic_Simulation/src/TrafficLight.h
+++ Concurrent_Traffic_Simulation/src/TrafficLight.h
@@ -30,17 +30,27 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+// Define the enum class TrafficLightPhase
+enum class TrafficLightPhase {red, green};
+
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
-
+    TrafficLight();
+//    ~TrafficLight();
     // getters / setters
 
     // typical behaviour methods
+//    void waitForGreen();
+//    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
+
     // typical behaviour methods
+//    void cycleThroughPhases();
+    TrafficLightPhase _currentPhase;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
